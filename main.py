4e803eb5533640bce9015e3b45a44b373e93a6bf
YadielRivera <56930780+YadielRivera@users.noreply.github.com>
2023-11-16 17:48:52 -0400

    menu

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def list_todos(todos):
     """
         List todos
     """
-
+    
+  
 def add_todo(todos):
     """
         Add todos
     """
+    todos.append()
 
 def edit_todo(todos):
     """
@@ -17,7 +19,8 @@ def delete_todo(todos):
     """
         Edit todos
     """
-
+  
+      
 def menu(todos):
     print("What would you like to do?")
     print("1. List todos")
@@ -26,7 +29,17 @@ def menu(todos):
     print("4. Delete a todo")
     print("5. Quit")
     choice = input("> ")
-
+    if choice == "1":
+      list_todos(1)
+    elif choice == "2":
+      list_todos(2)
+    elif choice == "3":
+      list_todos(3)
+    elif choice == "4":
+      list_todos(4)
+    elif choice == "5":
+      list_todos(5)
+      
 def main():
     print("Welcome to your todo list!")
     todos = []
